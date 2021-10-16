ed8d4ec810c537412c1614f07bee02a92ade6b18
Moritz Wilksch <moritz.wilksch@duke.edu>
2021-10-15 21:00:34 -0400

    code

--- code/10_write_to_db.py
+++ code/10_write_to_db.py
@@ -1,22 +1,29 @@
 #%%
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, String, MetaData, Date, Float, Integer, Enum
+from sqlalchemy import Table, Column, String, MetaData, Date, Float, Integer
 
 
 db = create_engine("postgresql://postgres:ThisIsSecretAF@localhost:5432")
 
-TICKER = "GME"
+TICKER = "AAPL"
 # EDGAR_CODE = "0000320193"
 
-ticker_cik_lookup = pd.read_csv("https://www.sec.gov/include/ticker.txt", sep="\t", header=None, names=["ticker", "cik"])
-EDGAR_CODE = f"{ticker_cik_lookup.query('ticker == @TICKER.lower()')['cik'].values[0]:0>10}"
+ticker_cik_lookup = pd.read_csv(
+    "https://www.sec.gov/include/ticker.txt",
+    sep="\t",
+    header=None,
+    names=["ticker", "cik"],
+)
+EDGAR_CODE = (
+    f"{ticker_cik_lookup.query('ticker == @TICKER.lower()')['cik'].values[0]:0>10}"
+)
 print(f"Using {EDGAR_CODE = }")
 
 
 #%%
-db_cols = ["ticker", "period_end", "value", "fy", "fp", "form", "filed", "item"]
-df_cols = ["ticker", "end", "val", "fy", "fp", "form", "filed", "item"]
+db_cols = ["ticker", "accn", "period_end", "value", "fy", "fp", "form", "filed", "item"]
+df_cols = ["ticker", "accn", "end", "val", "fy", "fp", "form", "filed", "item"]
 
 #%%
 # Create Table
@@ -24,14 +31,15 @@ meta = MetaData(db)
 stat_table = Table(
     "stats",
     meta,
-    Column("ticker", String),
-    Column("period_end", Date),
+    Column("accn", String),
+    Column("ticker", String, primary_key=True),
+    Column("period_end", Date, primary_key=True),
     Column("value", Float),
     Column("fy", Integer),
     Column("fp", String),
     Column("form", String),
     Column("filed", Date),
-    Column("item", String),
+    Column("item", String, primary_key=True),
 )
 
 stat_table.create(db, checkfirst=True)
@@ -59,14 +67,44 @@ for item in crawler.keys_to_extract:
             .rename({dfcol: dbcol for dfcol, dbcol in zip(df_cols, db_cols)}, axis=1)
         )
 
+        try:
+            with db.connect() as connection:
+                existing_pks = [
+                    x[0]
+                    for x in connection.execute(
+                        "SELECT DISTINCT ticker, period_end, item FROM stats;"
+                    )
+                ]
+        except:
+            existing_pks = []
+
+        eps = eps.query("not accn.isin(@existing_pks)").drop_duplicates(
+            ["ticker", "period_end", "item"]
+        )
+
         eps.to_sql("stats", con=db, if_exists="append", index=False)
-    except:
+
+    except KeyError:
         print(f"[WARN] {item} not found. Skipping it.")
+
 
 #%%
 stmt = stat_table.select()
 
-for x in db.execute(stmt).cursor:
-    print(x)
+with db.connect() as conn:
+    for x in conn.execute("SELECT * FROM stats;"):
+        print(x)
 
 #%%
+import seaborn as sns
+
+dfx = pd.read_sql(
+    """ 
+        SELECT period_end, extract(year from period_end) as year, fp, value as value
+        FROM stats
+        WHERE ticker = 'AAPL' AND item = 'Revenues'; 
+    """,
+    db,
+)
+
+sns.lineplot(data=dfx, x="period_end", y="value")
