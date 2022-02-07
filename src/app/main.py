0c139fd252fe23534c09f3d3841be88b90ea65d9
Moritz Wilksch <moritzwilksch@gmail.com>
2022-02-07 13:37:28 +0100

    add table + stub data

--- src/app/main.py
+++ src/app/main.py
@@ -18,11 +18,23 @@ app.mount(
 )
 
 stub = {
+    "companies": ["AAPL", "MSFT", "GOOG", "AMZN", "FB"],
+    "units": {
+        "Revenues": "USD",
+        "Expenses": "USD",
+        "Net Income": "USD",
+        "Dividend": "USD/share"
+    },
+
     "table_data": {
-        
+        "Revenues": {"AAPL": 42, "MSFT": 12, "GOOG": 3, "AMZN": 2, "FB": 1},
+        "Expenses": {"AAPL": 24, "MSFT": 5, "GOOG": 9, "AMZN": 8, "FB": 7},
+        "Net Income": {"AAPL": 18, "MSFT": -2, "GOOG": -3, "AMZN": -4, "FB": -5},
+        "Dividend": {"AAPL": 0.5, "MSFT": 0.2, "GOOG": 0.3, "AMZN": 0.4, "FB": 0.1}
+
     }
 }
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id})
+    return templates.TemplateResponse("index.html", {"request": request, "id": id, "data": stub})
