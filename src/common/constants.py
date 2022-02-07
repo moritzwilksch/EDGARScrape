3aceb47631b1e84d7a50d944ccf84a4e5bd67cb6
Moritz Wilksch <moritzwilksch@gmail.com>
2022-02-07 19:40:29 +0100

    start industry matching

--- src/common/constants.py
+++ src/common/constants.py
@@ -2,3 +2,4 @@
 FACTS_COLLECTION = "dev_facts"
 METRICS_COLLECTION = "dev_metrics"
 LOGS_COLLECTION = "scrape_logs"
+CIK_COLLECTION = "ciks"
---
+++ src/experiments/industry_matching.py
@@ -0,0 +1,26 @@
+from gc import collect
+from src.common.constants import CIK_COLLECTION
+import os
+from pymongo import MongoClient
+from rich import print
+
+# DB INIT
+mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+mongo_pass = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+client = MongoClient(f"mongodb://{mongo_user}:{mongo_pass}@localhost:27017/edgar?authSource=admin")
+db = client["edgar"]
+collection = db[CIK_COLLECTION]
+
+industries = list(collection.distinct("industry"))
+
+# print(sorted(industries))
+
+
+def get_same_industry(ticker: str):
+    query = {"ticker": ticker}
+    results = list(collection.find(query))
+    industries = set([item.get("industry") for item in results])
+    return list(collection.find({"industry": {"$in": list(industries)}}))
+
+print(get_same_industry("AAPL"))
