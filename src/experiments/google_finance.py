b3165993d2296b272a44ac965ad6d80541e0352b
Moritz Wilksch <moritzwilksch@gmail.com>
2022-02-09 18:04:32 +0100

    adding market cap to db works. TODO: refactor this

--- src/experiments/google_finance.py
+++ src/experiments/google_finance.py
@@ -7,7 +7,7 @@ from src.common.constants import CIK_COLLECTION
 import os
 from pymongo import MongoClient
 from rich import print
-
+import random
 # DB INIT
 mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
 mongo_pass = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
@@ -18,6 +18,10 @@ client = MongoClient(
 db = client["edgar"]
 collection = db[CIK_COLLECTION]
 
+with open("data/working_proxies.txt", "r") as f:
+    proxies = f.readlines()
+
+print(f"Found {len(proxies)} proxies")
 
 class MarketCapFetcher:
     def __init__(self, ticker: str):
@@ -26,7 +30,8 @@ class MarketCapFetcher:
         self.css_class = "P6K39c"
 
     def _get(self, exchange):
-        page = requests.get(f"{self.url}:{exchange}")
+        proxy = random.choice(proxies)
+        page = requests.get(f"{self.url}:{exchange}", proxies={"http": proxy})
 
         soup = BeautifulSoup(page.content, "lxml")
         try:
@@ -43,7 +48,10 @@ class MarketCapFetcher:
 
 
 # tickers = ["AAPL", "AMZN", "F", "FB", "GOOG", "JPM", "MPW", "MSFT", "TSLA"]
-tickers = collection.distinct("ticker")[:50]
+# tickers = collection.distinct("ticker")[:100]
+with open("data/sp500_tickers.txt") as f:
+    tickers = [t.strip() for t in f.readlines()]
+
 
 
 def get_market_cap(ticker: str):
@@ -54,9 +62,14 @@ def get_market_cap(ticker: str):
 
 
 tic = time.perf_counter()
-mcs = Parallel(prefer="threads", n_jobs=10)(
+mcs = Parallel(prefer="processes", n_jobs=10, verbose=100)(
     delayed(get_market_cap)(ticker) for ticker in tickers
 )
 tac = time.perf_counter()
 print(mcs)
 print(f"Took {tac-tic} seconds")
+
+
+for d in mcs:
+    if d.get("marketcap"):
+        collection.update_many({"ticker": d.get("ticker")}, {"$set": {"marketcap": d.get("marketcap")}})
--- src/experiments/proxy_fetcher.py
+++ src/experiments/proxy_fetcher.py
@@ -8,7 +8,6 @@ import re
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
     "Host": "www.sec.gov",
-
 }
 
 
@@ -26,9 +25,8 @@ try:
 except:
     print("No table found. Exiting.")
 
-df = df.query("Https == 'no'")#.query("Google == 'yes'")
+df = df.query("Https == 'no'")  # .query("Google == 'yes'")
 print(df)
-
 
 
 proxies = []
@@ -38,69 +36,34 @@ for _, ip, port, https in df[["IP Address", "Port", "Https"]].itertuples():
 
 print(f"Found {len(proxies)} proxies")
 
-# proxies = [Proxy(ip="196.0.111.194", port="34638")]
-
-
-
-
-
-
-# def is_working(proxy: Proxy):
-#     # print(f"----- {proxy.ip}:{proxy.port} -----")
-#     try:
-#         response = requests.get(
-#             # "http://api.ipify.org",
-#             # "http://ipinfo.io/ip",
-#             "https://www.google.com/",
-#             proxies={
-#                 "http" if proxy.https == "no" else "https": f"https://{proxy.ip}:{proxy.port}",
-#                 # "https": f"socks5://{proxy.ip}:{proxy.port}",
-#             },
-#             timeout=2,
-#             headers=headers,
-#         )
-#     except requests.exceptions.Timeout:
-#         print("Timeout")
-#         return False
-#     except requests.exceptions.ProxyError:
-#         print("Proxy error")
-#         return False
-#     except:
-#         print("Generic error")
-#         return False
-    
-#     if "google" in response.text and "robot" in response.text:
-#         return proxy
-#     else:
-#         print("Doesn't work")
-#         return False
 
 def is_working(proxy: Proxy):
     protocol = "http" if proxy.https == "no" else "https"
     try:
         response = requests.get(
             f"{protocol}://api.ipify.org",
-            proxies={
-                protocol: f"{protocol}://{proxy.ip}:{proxy.port}",
-            },
+            proxies={protocol: f"{protocol}://{proxy.ip}:{proxy.port}",},
             timeout=3,
             headers=headers,
         )
     except:
         return False
-    
+
     if re.match("^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$", response.text):
         print(f"{proxy.ip}:{proxy.port}")
         return proxy
-    
+
     return False
 
 
-
-working = Parallel(n_jobs=100, prefer="threads")(delayed(is_working)(proxy) for proxy in proxies)
+working = Parallel(n_jobs=100, prefer="threads")(
+    delayed(is_working)(proxy) for proxy in proxies
+)
 
 print(working)
 
-for p in working:
-    if p:
-        print(f"{p.ip}:{p.port}")
+
+with open("data/working_proxies.txt", "w") as f:
+    for p in working:
+        if p:
+            f.write(f"{'http' if p.https == 'no' else 'https'}://{p.ip}:{p.port}\n")
