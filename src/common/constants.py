ce39062d3e245b241aeec232f92c90af09f103fe
Moritz Wilksch <moritzwilksch@gmail.com>
2022-04-21 15:16:50 +0000

    rm hardcoded ip

--- src/common/constants.py
+++ src/common/constants.py
@@ -2,9 +2,10 @@ import os
 
 mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
 mongo_pass = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+mongo_ip = os.getenv("MONGO_IP")
 
 # Database related
-DB_CONNECTION_STRING = f"mongodb://{mongo_user}:{mongo_pass}@65.108.135.187:27017"
+DB_CONNECTION_STRING = f"mongodb://{mongo_user}:{mongo_pass}@{mongo_ip}:27017"
 AUTH_SOURCE = "edgar"
 
 FACTS_COLLECTION = "dev_facts"
